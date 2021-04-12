70a0dc5d29e01e610f341a841280db40adeca0f2
muditjain2000 <mudit2310jain@gmail.com>
2021-04-12 12:30:50 +0530

    fixed review bugs

--- src/components/review.jsx
+++ src/components/review.jsx
@@ -1,20 +1,25 @@
 import React from "react";
-import Form from "./common/form";
-import { getMovies } from "../services/movieService";
-import Joi from "joi-browser";
+import Form from './common/form';
+import { getMovies } from './../services/movieService';
+import  Joi  from 'joi-browser';
 import { review } from "../services/userService";
-import { currentUser } from "../services/authService";
-import { getReviews } from "../services/userService";
+import { currentUser } from './../services/authService';
+import { getReviews } from './../services/userService';
+import Rate from './common/rate';
+
+import RateShow from './rateShow';
 
 class Customers extends Form {
   state = {
-    data: { movie: [], review: "" },
-    user: "",
-    movies: [],
-    reviews: [],
+    data: {rating:"",movie:[],review:""},
+    user:"",
+    movies:[],
+    reviews:[],
     errors: {},
+    
   };
   schema = {
+    rating: Joi.number().required().label("Rating"),
     movie: Joi.string().required().label("Movie"),
     review: Joi.string().required().label("Review"),
   };
@@ -22,15 +27,17 @@ class Customers extends Form {
   async componentDidMount() {
     const { data: movies } = await getMovies();
     const { data: reviews } = await getReviews();
-    this.setState({ movies, reviews });
+    this.setState({ movies,reviews });
     const user = currentUser();
     this.setState({ user });
   }
 
   doSubmit = async () => {
     try {
-      await review(this.state.data, this.state.user);
+      await review(this.state.data,this.state.user);
       this.props.history.push("/movies");
+      
+      
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
@@ -40,64 +47,59 @@ class Customers extends Form {
     }
   };
 
+ 
+
   render() {
     const { user } = this.props;
     return (
-      <div id="review-main-wrapper">
-        <div id="review-wrapper">
-          <h1>Reviews</h1>
-          {user && (
-            <React.Fragment>
-              <form onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                  <label htmlFor="movie">Movie</label>
-                  <select
-                    name="movie"
-                    id="movie"
-                    onChange={this.handleChange}
-                    className="form-control"
-                  >
-                    <option value=""></option>
-                    {this.state.movies.map((option) => (
-                      <option
-                        key={option._id}
-                        value={[`${option._id}~${option.title}`]}
-                      >
-                        {option.title}
-                      </option>
-                    ))}
-                  </select>
-                  {/* {this.state.errors && <div className="alert alert-danger">{this.state.errors}</div>} */}
-                </div>
-                {/* <div className="mb-3">
+      
+    <div id="review-main-wrapper">
+      <div id="review-wrapper" >
+      <h1>Reviews</h1>
+      {user &&(
+        <React.Fragment>
+    <form onSubmit={this.handleSubmit}>
+    <div className="form-group">
+      <label htmlFor="rating">Rating</label><br/>
+    <Rate id="rating" onChange={this.handleChange} /></div>
+    <div className="form-group">
+      
+        <label htmlFor="movie">Movie</label>
+        <select name="movie" id="movie" onChange={this.handleChange} className="form-control">
+          <option value=""></option>
+          {this.state.movies.map((option) => (
+            <option key={option._id} value={[`${option._id}~${option.title}`]}>
+              {option.title}
+            </option>
+          ))}
+        </select>
+        {/* {this.state.errors && <div className="alert alert-danger">{this.state.errors}</div>} */}
+      </div>
+        {/* <div className="mb-3">
           <label for="exampleFormControlTextarea1" className="form-label">Review</label>
           <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={this.state.data[review]} onChange={this.handleChange}></textarea>
         </div> */}
-                {this.renderInput("review", "Review")}
-                {/* <button disabled={this.validate()} className="btn btn-primary">
+        {this.renderInput("review", "Review")}
+        {/* <button disabled={this.validate()} className="btn btn-primary">
         Save
       </button> */}
 
-                {this.renderButton("Save")}
-              </form>
-              <br />
-            </React.Fragment>
-          )}
-          <div>
-            <ul className="list-group">
-              {this.state.reviews.map((option) => (
-                <li className="list-group-item review-card" key={option._id}>
-                  <b>{option.movieName}</b>
-                  <br />~{option.userName}
-                  <br />
-                  {option.review}
-                </li>
-              ))}
-            </ul>
-          </div>
+           {this.renderButton("Save")} 
+        </form><br/>
+        
+
+        </React.Fragment>
+      )}
+      <div>
+        <ul className="list-group">
+        {this.state.reviews.map((option)=>(
+          
+          <li className="list-group-item review-card" key={option._id}><b>{option.movieName}</b><br/>{<RateShow number={option.rating}/>}<br/>~{option.userName}<br/>{option.review}</li>
+        ))}
+        </ul></div>
         </div>
-      </div>
-    );
+    </div>
+    )
   }
 }
 
